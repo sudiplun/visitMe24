4a32367a93e5e3a4f77dc21a20f596cc7e114955
lun <sudeeplun@gmail.com>
2024-01-17 20:06:46 +0545

    remove old

--- v0.2/js/dark.js
+++
@@ -1,9 +0,0 @@
-// Function to toggle the dark theme
-function toggleDarkMode() {
-  const htmlElement = document.documentElement;
-  htmlElement.classList.toggle("dark-mode");
-}
-
-// Event listener for the theme switch button
-const themeSwitchButton = document.getElementById("themeSwitchButton");
-themeSwitchButton.addEventListener("click", toggleDarkMode);
